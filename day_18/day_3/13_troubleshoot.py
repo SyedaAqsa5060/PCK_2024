958c9e879bf8e1102d6ac96e4aa8fc03c9c84922
Syed Jahangeer Shah <aqso2775@gmail.com>
2025-08-17 12:10:13 +0500

    Removed nested day folders inside day_18

---
+++ day_18/day_3/13_troubleshoot.py
@@ -0,0 +1,8 @@
+# Troobleshooting 
+
+#print(we are learning by codanics)        syntax error
+#print(25/0)                               runtime error
+# name ="aqsa"
+#print("hello name")                        symentic error 
+
+# trooble _ shooting is easy 
